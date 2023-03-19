75a6b07c44c8bb1290c7af5ab2dd48a982544ecb
BrunoJankowski <jankowskibruno2@gmail.com>
2023-03-19 23:36:39 +0100

    reset work

--- notes.js
+++ notes.js
@@ -2,7 +2,14 @@ var itemNotes = document.getElementsByClassName('notes')[0]
 itemNotes.value = localStorage.getItem('notes')
 
 var itemSumbit = document.getElementsByClassName('submit-notes')[0]
+var itemReset = document.getElementsByClassName('reset-notes')[0]
+
+itemReset.addEventListener('click', function(){
+    localStorage.setItem('notes', '...')
+    location.reload()
+})
 
 itemSumbit.addEventListener('click', function(){
     localStorage.setItem('notes', itemNotes.value)
+    location.reload()
 })
