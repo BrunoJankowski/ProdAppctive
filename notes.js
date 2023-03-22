705f15504a4260cae166159bb3405162d45fd6dc
BrunoJankowski <jankowskibruno2@gmail.com>
2023-03-22 20:57:32 +0100

    working names for links

--- notes.js
+++ notes.js
@@ -11,6 +11,7 @@ var itemLinksValue = document.getElementsByClassName('link-value')[0]
 var itemInfoNote = document.getElementsByClassName('info')[0]
 var itemAddLink = document.getElementsByClassName('add-links')[0]
 var itemLinksNames = document.getElementsByClassName('link-names')[0]
+console.log(itemLinksNames);
 
 var stored_links = JSON.parse(localStorage.getItem("links")); 
 var stored_names = JSON.parse(localStorage.getItem("link-names")); 
@@ -20,6 +21,7 @@ localStorage.setItem("link-names", JSON.stringify(stored_names));
 function reloadLinks() {
     for(var i = 0; i<itemLinks.length;i++){
         var linkText = itemLinks[i]
+        linkText.hidden = false
         linkText.innerHTML = '<a href="' + stored_links[i] + '" target=_blank class="linked">' + stored_names[i] + '<a/>'
         console.log(stored_links);
     }
@@ -28,11 +30,12 @@ function reloadLinks() {
 itemAddLink.addEventListener('click', function() {
     
     var link_value = itemLinksValue.value
-    var link_value = itemLinksNames.value
+    var link_name = itemLinksNames.value
 
     stored_links.push(link_value)
-    stored_names.push(link_value)
+    stored_names.push(link_name)
     localStorage.setItem("links", JSON.stringify(stored_links))
+    localStorage.setItem("link-names", JSON.stringify(stored_names))
     reloadLinks()
 })
 
